1c3f371a6b30976908b4d63e10fcbb68f22671b9
Domain314 <artjom.moisejev@hotmail.com>
2022-04-10 16:04:05 +0200

    added frontend-demos2

---
+++ v1/htdocs/frontend/frontend_demos/engine.ts
@@ -0,0 +1,20 @@
+//this big function means: load this function AFTER the document loading finished
+
+
+$(document).ready(function() {
+
+    
+
+    //CLOSE NEW APPOINTMENT FORM
+    $("#close_appointment_form").on('click', function () {
+        $("#appointment-form").fadeOut(300);
+        console.log("hiding appointment form");
+    });
+
+    //OPEN NEW APPOINTMENT FORM
+    $("#add_new_appointment").on('click', function () {
+        $("#appointment-form").fadeIn(300);
+        console.log("show appointment form");
+    });
+});
+
